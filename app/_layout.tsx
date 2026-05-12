a1a7082a11099e7350f031498044c1c1ed5de35f
Kian <kheijhackt@gmail.com>
2026-05-12 22:08:55 +0800

    Implemented conditional routing for auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,50 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
+import { useEffect, useState } from "react";
+import * as authEndpoints from "../services/authEndpoints";
+import * as authStore from "../utils/authStore";
 
 export default function RootLayout() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const router = useRouter();
+  const segments = useSegments();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const token = await authStore.getToken();
+      if (token === null) {
+        setIsAuthenticated(false);
+        setIsLoading(false);
+        return;
+      }
+      const response = await authEndpoints.verifyMe();
+      setIsAuthenticated(response);
+      setIsLoading(false);
+    };
+    checkAuth();
+  }, []);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    const inTabsGroup = segments[0] === "(tabs)";
+    if (!isAuthenticated && inTabsGroup) {
+      router.replace("/(auth)/googleAuth");
+    } else if (isAuthenticated && !inTabsGroup) {
+      router.replace("/(tabs)/home");
+    }
+  }, [isAuthenticated, isLoading]);
+
+  if (isLoading) return null;
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" />
+      {isAuthenticated ? (
+        <Stack.Screen name="(tabs)" />
+      ) : (
+        <Stack.Screen name="(auth)/googleAuth" />
+      )}
     </Stack>
   );
 }
